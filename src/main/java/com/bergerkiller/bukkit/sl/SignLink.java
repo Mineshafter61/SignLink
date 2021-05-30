62e0a32e015cf0e71c2d22a1e852e2fb322572b9
Mineshafter61 <yuhang.061@gmail.com>
2021-05-30 12:55:18 +0900

    Now Signlink won't spam console! hurhur

--- src/main/java/com/bergerkiller/bukkit/sl/SignLink.java
+++ src/main/java/com/bergerkiller/bukkit/sl/SignLink.java
@@ -81,7 +81,7 @@ public class SignLink extends PluginBase {
      * Whether all signs on the server are routinely checked for sign text changes.
      * When a sign suddenly has a variable displayed on it, this will make it change that
      * into a variable.
-     * 
+     *
      * @return True when sign changes are automatically discovered
      */
     public boolean discoverSignChanges() {
@@ -188,7 +188,7 @@ public class SignLink extends PluginBase {
 
         //General %time% and %date% update thread
         timetask = new TimeUpdateTask(this).start(5, 5);
-        
+
         loadValues();
 
         //Start updating
@@ -248,7 +248,7 @@ public class SignLink extends PluginBase {
 
     public void loadValues() {
         FileConfiguration values = new FileConfiguration(this, "values.yml");
-        if (!values.exists()) {            
+        if (!values.exists()) {
             values.set("test.ticker", "LEFT");
             values.set("test.tickerInterval", 3);
             values.set("test.value", "This is a test message being ticked from right to left. ");
@@ -313,7 +313,7 @@ public class SignLink extends PluginBase {
 
     /**
      * Removes a given variable from the player editing map
-     * 
+     *
      * @param variable to remove
      */
     public void removeEditing(Variable variable) {
@@ -364,14 +364,16 @@ public class SignLink extends PluginBase {
 
     @Override
     public boolean command(CommandSender sender, String cmdLabel, String[] args) {
+      // Make Signlink not spam console
+        boolean message = sender instanceof Player;
         // Toggle sign updating on/off
         if (cmdLabel.equalsIgnoreCase("togglesignupdate")) {
             Permission.TOGGLEUPDATE.handle(sender);
             updateSigns = !updateSigns;
             if (updateSigns) {
-                sender.sendMessage(ChatColor.GREEN + "Signs are now being updated!");
+                if (message) sender.sendMessage(ChatColor.GREEN + "Signs are now being updated!");
             } else {
-                sender.sendMessage(ChatColor.YELLOW + "Signs are now no longer being updated!");
+                if (message) sender.sendMessage(ChatColor.YELLOW + "Signs are now no longer being updated!");
             }
             return true;
         }
@@ -380,15 +382,15 @@ public class SignLink extends PluginBase {
             Permission.RELOAD.handle(sender);
             loadValues();
             loadSigns();
-            sender.sendMessage(ChatColor.GREEN + "SignLink reloaded the Variable values");
+            if (message) sender.sendMessage(ChatColor.GREEN + "SignLink reloaded the Variable values");
             return true;
         }
         // Remaining variable commands
         if (!cmdLabel.equalsIgnoreCase("variable") && !cmdLabel.equalsIgnoreCase("var")) {
             return false;
         } else if (args.length == 0) {
-            sender.sendMessage(ChatColor.RED + "Please specify a sub-command!");
-            sender.sendMessage(ChatColor.YELLOW + "Use /help variable for command help");
+            if (message) sender.sendMessage(ChatColor.RED + "Please specify a sub-command!");
+            if (message) sender.sendMessage(ChatColor.YELLOW + "Use /help variable for command help");
             return true;
         }
         // All below commands are sub-commands of /variable
@@ -436,15 +438,15 @@ public class SignLink extends PluginBase {
             }
             // Remove
             if (allVars.isEmpty()) {
-                sender.sendMessage(ChatColor.YELLOW + "No variables were found that could be deleted");
+                if (message) sender.sendMessage(ChatColor.YELLOW + "No variables were found that could be deleted");
             } else {
                 for (Variable var : allVars) {
                     Variables.remove(var.getName());
                 }
                 if (allVars.size() == 1) {
-                    sender.sendMessage(ChatColor.GREEN + "One variable was deleted: " + allVars.get(0).getName());
+                    if (message) sender.sendMessage(ChatColor.GREEN + "One variable was deleted: " + allVars.get(0).getName());
                 } else {
-                    sender.sendMessage(ChatColor.GREEN + Integer.toString(allVars.size()) + " variables have been deleted!");
+                    if (message) sender.sendMessage(ChatColor.GREEN + Integer.toString(allVars.size()) + " variables have been deleted!");
                 }
             }
             return true;
@@ -454,12 +456,12 @@ public class SignLink extends PluginBase {
         if (cmdLabel.equalsIgnoreCase("refreshpapi")) {
             Permission.REFRESHPAPI.handle(sender);
             if (this.papi == null) {
-                sender.sendMessage(ChatColor.RED + "PlaceholderAPI is not installed or enabled in SignLink");
+                if (message) sender.sendMessage(ChatColor.RED + "PlaceholderAPI is not installed or enabled in SignLink");
             } else {
                 for (Player player : Bukkit.getOnlinePlayers()) {
                     this.papi.refreshVariables(player);
                 }
-                sender.sendMessage(ChatColor.GREEN + "All PlaceholderAPI variables have been updated!");
+                if (message) sender.sendMessage(ChatColor.GREEN + "All PlaceholderAPI variables have been updated!");
             }
             return true;
         }
@@ -469,7 +471,7 @@ public class SignLink extends PluginBase {
             if (args.length >= 1) {
                 Permission.EDIT.handle(sender, args[0]);
                 if (args[0].contains(" ")) {
-                    sender.sendMessage(ChatColor.RED + "Variable names can not contain spaces!");
+                    if (message) sender.sendMessage(ChatColor.RED + "Variable names can not contain spaces!");
                     return true;
                 }
                 // Add a new Variable editing slot
@@ -480,9 +482,9 @@ public class SignLink extends PluginBase {
                     editingvars.put(null, edit);
                 }
                 // Handle feedback
-                sender.sendMessage(ChatColor.GREEN + "You are now editing variable '" + args[0] + "'");
+                if (message) sender.sendMessage(ChatColor.GREEN + "You are now editing variable '" + args[0] + "'");
             } else {
-                sender.sendMessage(ChatColor.RED + "Please specify a variable name!");
+                if (message) sender.sendMessage(ChatColor.RED + "Please specify a variable name!");
             }
             args = StringUtil.remove(args, 0);
             if (args.length == 0) {
@@ -499,7 +501,7 @@ public class SignLink extends PluginBase {
             var = editingvars.get(null);
         }
         if (var == null) {
-            sender.sendMessage(ChatColor.RED + "Please edit a variable first using /variable edit!");
+            if (message) sender.sendMessage(ChatColor.RED + "Please edit a variable first using /variable edit!");
             return true;
         }
 
@@ -508,23 +510,23 @@ public class SignLink extends PluginBase {
         if (cmdLabel.equalsIgnoreCase("for") || cmdLabel.equalsIgnoreCase("forplayers")) {
             var.players = args;
             if (args.length == 0) {
-                sender.sendMessage(ChatColor.GREEN + "You are now editing this variable for all players!");
+                if (message) sender.sendMessage(ChatColor.GREEN + "You are now editing this variable for all players!");
             } else {
-                sender.sendMessage(ChatColor.GREEN + "You are now editing this variable for the selected players!");
+                if (message) sender.sendMessage(ChatColor.GREEN + "You are now editing this variable for the selected players!");
             }
         } else if (cmdLabel.equalsIgnoreCase("get")) {
             if (var.global()) {
-                sender.sendMessage(ChatColor.YELLOW + "Current value is: " + ChatColor.BLACK + var.variable.getDefault());
+                if (message) sender.sendMessage(ChatColor.YELLOW + "Current value is: " + ChatColor.BLACK + var.variable.getDefault());
             } else {
-                sender.sendMessage(ChatColor.YELLOW + "Current value is: " + ChatColor.BLACK + var.variable.get(var.players[0]));
+                if (message) sender.sendMessage(ChatColor.YELLOW + "Current value is: " + ChatColor.BLACK + var.variable.get(var.players[0]));
             }
         } else if (cmdLabel.equalsIgnoreCase("setdefault") || cmdLabel.equalsIgnoreCase("setdef")) {
             String value = StringUtil.ampToColor(StringUtil.join(" ", args));
             var.variable.setDefault(value);
             if (args.length == 0) {
-                sender.sendMessage(ChatColor.YELLOW + "Default variable value emptied!");
+                if (message) sender.sendMessage(ChatColor.YELLOW + "Default variable value emptied!");
             } else {
-                sender.sendMessage(ChatColor.YELLOW + "Default variable value set to '" + value + ChatColor.RESET + ChatColor.YELLOW + "'!");
+                if (message) sender.sendMessage(ChatColor.YELLOW + "Default variable value set to '" + value + ChatColor.RESET + ChatColor.YELLOW + "'!");
             }
         } else if (cmdLabel.equalsIgnoreCase("set")) {
             if (args.length == 0) {
@@ -533,7 +535,7 @@ public class SignLink extends PluginBase {
                 } else {
                     var.group().set("");
                 }
-                sender.sendMessage(ChatColor.YELLOW + "Variable value emptied!");
+                if (message) sender.sendMessage(ChatColor.YELLOW + "Variable value emptied!");
             } else {
                 String value = StringUtil.ampToColor(StringUtil.join(" ", args));
                 if (var.global()) {
@@ -541,7 +543,7 @@ public class SignLink extends PluginBase {
                 } else {
                     var.group().set(value);
                 }
-                sender.sendMessage(ChatColor.YELLOW + "Variable value set to '" + value + ChatColor.RESET + ChatColor.YELLOW + "'!");
+                if (message) sender.sendMessage(ChatColor.YELLOW + "Variable value set to '" + value + ChatColor.RESET + ChatColor.YELLOW + "'!");
             }
         } else if (cmdLabel.equalsIgnoreCase("clear")) {
             if (var.global()) {
@@ -549,7 +551,7 @@ public class SignLink extends PluginBase {
             } else {
                 var.group().clear();
             }
-            sender.sendMessage(ChatColor.YELLOW + "Variable has been cleared!");
+            if (message) sender.sendMessage(ChatColor.YELLOW + "Variable has been cleared!");
         } else if (cmdLabel.equals("addpause") || cmdLabel.equalsIgnoreCase("pause")) {
             if (args.length == 2) {
                 try {
@@ -562,12 +564,12 @@ public class SignLink extends PluginBase {
                         t = var.group().getTicker();
                     }
                     t.addPause(delay, duration);
-                    sender.sendMessage(ChatColor.GREEN + "Ticker pause added!");
+                    if (message) sender.sendMessage(ChatColor.GREEN + "Ticker pause added!");
                 } catch (Exception ex) {
-                    sender.sendMessage(ChatColor.RED + "Please specify valid pause delay and duration values!");
+                    if (message) sender.sendMessage(ChatColor.RED + "Please specify valid pause delay and duration values!");
                 }
             } else {
-                sender.sendMessage(ChatColor.RED + "Please specify the delay and duration for this pause!");
+                if (message) sender.sendMessage(ChatColor.RED + "Please specify the delay and duration for this pause!");
             }
         } else if (cmdLabel.equalsIgnoreCase("clearpauses") || cmdLabel.equalsIgnoreCase("clearpause")) {
             Ticker t;
@@ -577,14 +579,14 @@ public class SignLink extends PluginBase {
                 t = var.group().getTicker();
             }
             t.clearPauses();
-            sender.sendMessage(ChatColor.YELLOW + "Ticker pauses cleared!");
+            if (message) sender.sendMessage(ChatColor.YELLOW + "Ticker pauses cleared!");
         } else if (cmdLabel.equalsIgnoreCase("delete")) {
             if (var.variable.getSigns().length == 0 || args.length > 0 && args[0].equalsIgnoreCase("force")) {
                 Variables.remove(var.variable.getName());
-                sender.sendMessage(ChatColor.GREEN + "Deleted variable '" + var.variable.getName() + "'!");
+                if (message) sender.sendMessage(ChatColor.GREEN + "Deleted variable '" + var.variable.getName() + "'!");
             } else {
-                sender.sendMessage(ChatColor.RED + "This variable still contains signs that are displaying it!");
-                sender.sendMessage(ChatColor.YELLOW + "To delete it anyhow, use /variable delete force");
+                if (message) sender.sendMessage(ChatColor.RED + "This variable still contains signs that are displaying it!");
+                if (message) sender.sendMessage(ChatColor.YELLOW + "To delete it anyhow, use /variable delete force");
             }
         } else if (cmdLabel.equalsIgnoreCase("setticker")) {
             if (args.length >= 1) {
@@ -620,17 +622,17 @@ public class SignLink extends PluginBase {
                 }
                 t.reset(varValue.get());
                 if (!t.isTicking()) {
-                    sender.sendMessage(ChatColor.GREEN + "Ticker is disabled");
+                    if (message) sender.sendMessage(ChatColor.GREEN + "Ticker is disabled");
                 } else {
-                    sender.sendMessage(ChatColor.GREEN + "You set a '" + t.getMode().toString().toLowerCase()
+                    if (message) sender.sendMessage(ChatColor.GREEN + "You set a '" + t.getMode().toString().toLowerCase()
                             + "' ticker ticking every " + t.getInterval() + " ticks!");
                 }
             } else {
-                sender.sendMessage(ChatColor.RED + "Please specify the ticker direction!");
+                if (message) sender.sendMessage(ChatColor.RED + "Please specify the ticker direction!");
             }
         } else {
-            sender.sendMessage(ChatColor.RED + "Unknown sub-command: " + ChatColor.YELLOW + cmdLabel);
-            sender.sendMessage(ChatColor.YELLOW + "Use /help variable for command help");
+            if (message) sender.sendMessage(ChatColor.RED + "Unknown sub-command: " + ChatColor.YELLOW + cmdLabel);
+            if (message) sender.sendMessage(ChatColor.YELLOW + "Use /help variable for command help");
         }
          return true;
     }
